34f2d458052630817231ffdfd0015154a5985d30
Victorsavian <victorssneves@gmail.com>
2025-05-22 23:19:27 -0300

    Inclusão do Formulário de Contato
    Inclusão do formulário com recebimento por email, automático do FormSubmit.
    Após envio, direcionado para pagina de agradecimento, mas texto descrito pelo Javascript, ainda não está funcionando.

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ function apaga() {
     var img = document.querySelector('img#img')
     img.style.border = '5px solid rgb(26, 25, 25)'
 }
-    
+
+function obrigado() {
+    resp = document.querySelector('div.respemail')
+    user = document.querySelector('input.nomecontato')
+    resp.innerHTML = `${user}, assim que possível vou ler seu recado \u128140`
+}
